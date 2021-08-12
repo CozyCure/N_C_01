7797d3cc6b1011e2cc3ef212b4f3b3d4c443e112
CozyCure <88157441+CozyCure@users.noreply.github.com>
2021-08-12 20:44:36 +0900

    Delete Momentum directory

--- Momentum/js/background.js
+++
@@ -1,9 +0,0 @@
-const images = [
-    "0.jpg", "1.jpg", "2.jpg"
-];
-function setBGImages () {
-const url = "url(img/"+images[Math.floor(Math.random() * images.length)]+")";
-
-document.getElementById("bg").style.backgroundImage = url;
-};
-setBGImages();
